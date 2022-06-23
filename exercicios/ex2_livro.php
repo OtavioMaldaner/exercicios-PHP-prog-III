b819791d26fae7172b86c77301e998546b740807
Otávio Maldaner <otaviomaldaner571@gmail.com>
2022-06-23 11:28:58 -0300

    exercicio 2

---
+++ exercicios/ex2_livro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 2</title>
+</head>
+<body>
+    <h1>Exercício 2</h1>
+    <h3>Processando o código: variáveis</h3>
+    <p>Ao executar os códigos a seguir, escreva como ficará o resultado na tela do navegador</p>
+    <div>
+        <h3>Exercício a:</h3>
+        <?php
+        echo"Olá";
+        ?>
+        <!-- O código printa a mensagem "Olá" na tela -->
+    </div>
+    <div>
+        <h3>Exercício b:</h3>
+        <?php 
+        echo"Olá";
+        echo"Pessoal";
+        ?>
+        <!-- O código printa a mensagem "OláPessoal" -->
+    </div>
+    <div>
+        <h3>Exercício c:</h3>
+        <?php
+        echo"Olá<br>";
+        echo"Pessoal";
+        ?>
+        <!-- O código printa a mensagem "Olá Pessoal" com quebra de linha -->
+    </div>
+
+</body>
+</html>
