efab6f9f3ed3c77fbf3f3fc7cb985b9a014453fc
Otávio Maldaner <otaviomaldaner571@gmail.com>
2022-06-23 11:52:25 -0300

    upload

---
+++ exercicios/ex3_livro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 3</title>
+</head>
+<body>
+<h1>Exercício 3</h1>
+    <h3>Processando o código: operadores aritméticos</h3>
+    <p>Ao executar os códigos a seguir, escreva como ficará o resultado na tela do navegador</p>
+    <div>
+        <h3>Exercício a:</h3>
+        <?php
+        $x = 1;
+        $x = -$x;
+        $y = $x;
+        echo $x."<br>".$y;
+        ?>
+        <!-- O código printa a mensagem "Olá" na tela -->
+    </div>
+    
+</body>
+</html>
