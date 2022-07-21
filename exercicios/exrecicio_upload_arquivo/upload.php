e8a4586754de47004951b8938654ab3643b2d6ec
Otávio Maldaner <otaviomaldaner571@gmail.com>
2022-07-21 09:52:01 -0300

    up

--- exercicios/exrecicio_upload_arquivo/upload.php
+++ exercicios/exrecicio_upload_arquivo/upload.php
@@ -1,9 +1,27 @@
 <?php
+// print_r($_FILES['arquivo']);
+// foreach($_FILES['arquivo'] as $arquivo){
+// var_dump($arquivo);
 $diretorio = "arquivos/";
+$diretorioImagens = 'imagens/';
 $nome_arquivo = $_FILES['arquivo']['name'];
-if(move_uploaded_file($_FILES["arquivo"]["tmp_name"],$diretorio.$nome_arquivo)){
-    echo "Upload realizado com sucesso.";
-}else{
-    echo "Upload não foi realizado.";
+$info_name = explode(".",$nome_arquivo);
+$extensao = end($info_name);
+$extensao = strtolower($extensao);
+// var_dump($extensao);
+if($extensao == 'png'){
+    if(move_uploaded_file($_FILES['arquivo']["tmp_name"],$diretorioImagens.uniqid().".".$extensao)){
+        echo "Upload ".$nome_arquivo." realizado com sucesso no diretório ".$diretorioImagens.".";
+    } else {
+        echo "Upload não foi realizado.";
+    }
+} else {
+    if(move_uploaded_file($_FILES['arquivo']["tmp_name"],$diretorio.uniqid().".".$extensao)){
+        echo "Upload ".$nome_arquivo." realizado com sucesso no diretório ".$diretorio.".";
+    } else {
+        echo "Upload não foi realizado.";
+    }
 }
+    // }
+
 ?>
